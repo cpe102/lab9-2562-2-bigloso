2a331fed01bc1be86094492596bedeea09814814
Bigloso <wayside.big@gmail.com>
2020-01-28 16:44:05 +0700

    asdasd

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,21 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include<fstream>
+#include<iostream>
+#include<string>
+using namespace std ;
+
+int main(){
+    ifstream f1("cheerbook.txt");
+    ofstream f2("cheerbook_copy.txt");
+    string s;
+    f2<<"-------------------- SOTUS ---------------------"<<endl;
+    while(getline(f1,s)){
+        f2<<s<<endl ;
+    }
+    f2<<"-------------------- SOTUS ---------------------";
+    f1.close();
+    
+    return 0 ;
+}
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,30 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+using namespace std ;
+
+int main(){
+    int count =0 ;
+    float sum =0 ;
+    float asd , mean ;
+    string textline ;
+    ifstream source("score.txt") ;
+    while (getline(source,textline))
+    {
+        cout << textline << endl ;
+        sum += atof(textline.c_str()) ;
+        asd +=pow(atof(textline.c_str()),2);
+        count++ ;
+    }
+    
+    cout << "Number of data = "<<count <<endl ;
+    cout << "Mean = " << sum/count  << endl;
+    cout << "Standard deviation = " << sqrt(asd/count-pow((1/count)*sum,2)) ;
+    source.close();
+    return 0 ;
+}
